5efebbc658c097cbbe986eaa2481429388c9a5ac
tom <tom@sapletta.com>
2021-06-19 10:17:46 +0200

    update for multi path *

--- let_path.php
+++ let_path.php
@@ -3,108 +3,72 @@
 // 'https://php.letpath.com/let_path.php',
 //namespace LetPath;
 
-
-function getDirContents($dir, &$results = array())
-{
-    $files = scandir($dir);
-
-    foreach ($files as $key => $value) {
-        $path = realpath($dir . DIRECTORY_SEPARATOR . $value);
-        if (!is_dir($path)) {
-            $results[] = $path;
-        } else if ($value != "." && $value != "..") {
-            getDirContents($path, $results);
-            $results[] = $path;
-        }
-    }
-
-    return $results;
-}
-
 /**
- * @param $path
+ * @param $dir
  * @param null $callback
- * @return string
+ * @param array $results
+ *
+ * @return array|mixed
  *
  * @throws Exception
  */
-function let_path($path, $callback = null)
+function let_path($dir, $callback = null, &$results = array())
 {
-    $separator = '*';
+	$sep = '*';
+	$parts = explode($sep, $dir);
+	$size = count($parts);
 
-    if (empty($path)) {
-        throw new Exception("path: $path is empty");
-    }
-
-    $path_list = [];
-    if (!is_array($path)) {
-        $path_list[] = $path;
-    } else {
-        $path_list = $path;
-    }
-
-    $txt = [];
-    foreach ($path_list as $path_item) {
-        $parts = (explode($separator, $path_item));
-
-        if ($parts >= 1) {
-
-            // TODO: get first occurence
-            // if more make it later in the next time
-            // TODO: make recursive, if more than one
-
-            $path_prefix = $parts[0];
-            $path_suffix = $parts[1];
-//        $files = scandir($path);
-            $files = array_diff(scandir($path_prefix), array('.', '..'));
-
-            foreach ($files as $path_subitem) {
+	// IF one or more * in the path
+	if ($size === 2) {
+		$path_prefix = $parts[0];
+		$path_suffix = $parts[1];
+		$files = scandir($path_prefix);
+	} else if ($size > 2) {
+		$path_prefix = (string)$parts[0];
+		unset($parts[0]);
+		$path_suffix = (string)implode($sep, $parts);
+		$files = scandir($path_prefix);
+	} else {
+		$files = scandir($dir);
+	}
 
 
-//                $path_subitem = str_replace("*", $path_subitem, $path_item);
-                $path_subitem = $path_prefix . $path_subitem . $path_suffix;
-//            var_dump($path_subitem);
+	foreach ($files as $key => $value) {
 
-                // check if path exist
-                if (!path_exists($path_subitem)) {
-//			throw new Exception("path: " . $path_item . " not exist ");
-                    continue;
-                }
+		$path = $dir . DIRECTORY_SEPARATOR . $value;
 
-                if (is_callable($callback)) {
-                    $callback($path_subitem);
-//                    continue;
-                }
+		if ($size > 2) {
 
-//            echo "<a href='$file'>$file</a>";
-                $txt[] = $path_subitem;
+			$path = $path_prefix . $value . $path_suffix;
+			let_path($path, $callback, $results);
 
-            }
-        }
+		} else {
 
-        // check if path exist
-        if (!path_exists($path_item)) {
-//			throw new Exception("path: " . $path_item . " not exist ");
-            continue;
-        }
+			if ($size === 2) {
+				$path = $path_prefix . $value . $path_suffix;
+			}
 
-        // Check Content
-//		$file = file_get_contents($path, true);
-//		if (empty($file)) {
-//			throw new Exception("Content from path: $path is empty");
-//            continue;
-//        }
+			if ($value != "." && $value != "..") {
 
-        if (is_callable($callback)) {
-            $callback($path_item);
-//            continue;
-        }
+				if (!is_dir($path)) {
+					// ONLY EXISTING FILES
+					if (file_exists($path)) {
+						$results[] = $path;
 
-        $txt[] = $path_item;
-    }
+						if (is_callable($callback)) {
+							$callback($path);
+						}
+					}
+				} else {
+					let_path($path, $callback, $results);
+				}
+			}
 
+		}
 
-    return $txt;
+	}
+
+	return $results;
 }
 
 
@@ -113,39 +77,39 @@ function let_path($path, $callback = null)
  */
 class LetPath
 {
-    /** @var array|mixed */
-    public $json = [];
+	/** @var array|mixed */
+	public $json = [];
 
-    /** @var string */
-    public $path = '';
+	/** @var string */
+	public $path = '';
 
-    /**
-     * LetPath constructor.
-     * @param $path
-     */
-    function __construct($path)
-    {
-        $this->path = $path;
-        $this->json = let_path($path);
-    }
+	/**
+	 * LetPath constructor.
+	 * @param $path
+	 */
+	function __construct($path)
+	{
+		$this->path = $path;
+		$this->json = let_path($path);
+	}
 
-    /**
-     * @return mixed
-     */
-    function first()
-    {
-        return $this->json[0];
-    }
+	/**
+	 * @return mixed
+	 */
+	function first()
+	{
+		return $this->json[0];
+	}
 
-    /**
-     * @param $callback
-     */
-    function each($callback)
-    {
-        foreach ($this->json as $item) {
-            $callback($item);
-        }
-    }
+	/**
+	 * @param $callback
+	 */
+	function each($callback)
+	{
+		foreach ($this->json as $item) {
+			$callback($item);
+		}
+	}
 }
 
 
@@ -155,9 +119,9 @@ class LetPath
  */
 function path_exists($path)
 {
-    if (file_exists($path) === false) {
-        return false;
-    }
+	if (file_exists($path) === false) {
+		return false;
+	}
 
-    return true;
+	return true;
 }
